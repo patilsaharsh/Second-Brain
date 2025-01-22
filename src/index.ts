c10d000790d94af4613d720739fdf8b1621330ae
saharsh patil <saharshap3@gmail.com>
2025-01-22 22:27:14 +0530

    Added v1 progress

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,110 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { ContentModel, UserModel } from "./db";
+import { JWT_PASSWORD } from "./config";
+import { userMiddleware } from "./middleware";
+
+
 
 const app = express();
+app.use(express.json())
 
-app.post("/api/v1/signup", function (req, res) {
+app.post("/api/v1/signup",async function (req, res) {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    await UserModel.create({
+      username:username,
+      password:password
+    })
+  
+    res.json({
+      messsage:"User signed up"
+    })
+  
+    
+  } catch (e) {
+
+    res.status(411).json({
+      message:"User already exists"
+    })
+    
+  }
+
+  
+});
+
+app.post("/api/v1/signin",async function (req, res) {
+  const username = req.body.username
+  const password = req.body.password
+  const existingUser = await UserModel.findOne({
+    username,
+    password
+  })
+
+  if(existingUser){
+    const token = jwt.sign({
+      id:existingUser._id
+    },JWT_PASSWORD)
+    res.json({
+      token
+    })
+  }else{
+    res.status(403).json({
+      message:"Incorrect credentials"
+    })
+  }
+  
+
 
 });
 
-app.post("/api/v1/signin", function (req, res) {
+app.post("/api/v1/content",userMiddleware, async function (req, res) {
+  const link = req.body.link;
+  const type = req.body.type;
+
+  await ContentModel.create({
+    link,
+    type,
+    //@ts-ignore
+    userId:req.userId,
+    tags:[]
+  })
+
+  res.json({
+    message:"Content added"
+  })
 
 });
 
-app.get("/api/v1/content", function (req, res) {
+app.get("/api/v1/content",userMiddleware, async function (req, res) {
+ //@ts-ignore
+ const  userId = req.userId ;
+ const content = await ContentModel.find({
+  userId:userId
+ }).populate("userId","username")
+
+ res.json({
+  content
+ })
 
 });
 
-app.delete("/api/v1/content", function (req, res) {
+app.delete("/api/v1/content",userMiddleware,async function (req, res) {
+  const contentId = req.body.contentId;
+
+  await ContentModel.deleteMany({
+    contentId,
+    //@ts-ignore
+    userId :req.userId
+
+  })
+  res.json({
+    message:"Deleted"
+  })
 
 });
 
@@ -27,3 +115,7 @@ app.post("/api/v1/share", function (req, res) {
 app.get("/api/v1/:shareLink", function (req, res) {
   
 });
+
+app.listen(3000,function(){
+ console.log("app is listening at port 3000")
+})
